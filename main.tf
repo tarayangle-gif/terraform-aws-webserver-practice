c1842ea3d9de69dbd9ef426b49c3ca7f63d317e5
Tatsuya_Enjinia <tarayangle@gmail.com>
2026-07-29 22:52:26 +0900

    Configure S3 backend for shared tfstate management

--- main.tf
+++ main.tf
@@ -6,12 +6,17 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "sanndeimandeityuusudeiamaama"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
 }
-
 # 1. VPCの作成
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
